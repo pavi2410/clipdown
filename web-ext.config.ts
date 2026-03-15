078f0f0ccafaaa155526e01f4afa023eeaefa534
Pavitra Golchha <hello@pavi2410.com>
2026-03-15 16:37:02 +0530

    feat: add web extension configuration file for Chromium arguments

---
+++ web-ext.config.ts
@@ -0,0 +1,9 @@
+import { defineWebExtConfig } from 'wxt';
+
+export default defineWebExtConfig({
+  chromiumArgs: [
+    'https://developer.chrome.com/docs/extensions/get-started',
+    'https://base-ui.com/react/components/accordion',
+    'https://en.wikipedia.org/wiki/Zenobia',
+  ],
+});
